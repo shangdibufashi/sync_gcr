7413984cbbc9e69558b0acded4ae8ad9e7dcec95
pinguo-liuhongwei <liuhongwei@camera360.com>
2019-03-08 18:36:51 +0800

    update

--- image/image_process.sh
+++ image/image_process.sh
@@ -5,8 +5,9 @@ MY_REPO=jiapinai
 hub_tag_exist(){
 	if [ -f "cache/${MY_REPO}.$1.$2" ]; then
 		echo 1
+	else
+	    curl -s https://hub.docker.com/v2/repositories/${MY_REPO}/$1/tags/$2/ | jq -r .name 
 	fi 
-    curl -s https://hub.docker.com/v2/repositories/${MY_REPO}/$1/tags/$2/ | jq -r .name
 }
 
 image_pull(){
@@ -22,11 +23,13 @@ image_prepare(){
 	tag=`echo "$1" | awk -F':' '{print \$2}'`
 	img_name=$(echo "$repo" | sed 's/\//./g' )
 	target="$MY_REPO/$img_name"
-	if [ "$( hub_tag_exist $img_name $tag )" == null ]; then
+	exists=$( hub_tag_exist "$img_name" "$tag" )
+	echo "$repo => $img_name $tag $exists"
+	if [ "$exists" == null ]; then
 		echo "$repo:$tag => $target:$tag"
 		image_pull "$repo:$tag" "$target:$tag"
 	else
-		echo "ignored $1"
+		echo "ignored [$exists] $1"
 	fi
 	echo "cache/${MY_REPO}.$img_name.$tag" > "cache/${MY_REPO}.$img_name.$tag"
 }
